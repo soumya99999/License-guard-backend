cf1c09c645309a93c807e3874b90480e715bf7f7
SomberCoderSRB <123350502+soumya99999@users.noreply.github.com>
2025-07-11 23:31:27 +0530

    created backend with springboot

---
+++ backend/src/main/java/com/project/sasken/License/Guard/LicenseGuardApplication.java
@@ -0,0 +1,13 @@
+package com.project.sasken.License.Guard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseGuardApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/project/sasken/License/Guard/LicenseGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sasken.License.Guard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
